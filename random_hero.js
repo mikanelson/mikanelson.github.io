843202e539e09dd598cf7b275acc5b7fa172e405
Mika Nelson <mika_nelson39@mymail.eku.edu>
2020-12-25 00:56:40 -0500

    Added image to hero api all.json file, started webpage design

--- random_hero.js
+++ random_hero.js
@@ -2,22 +2,21 @@ function Get(yourUrl) {
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",yourUrl,false);
     Httpreq.send(null);
-    return Httpreq.responseText;          
+    return Httpreq.responseText;
 }
 
 var json = Get("https://mikanelson.github.io/heroes/all.json");
 var parsedJson = JSON.parse(json);
 
 var keys = Object.keys(parsedJson);
-var randomHero = parsedJson[keys[ keys.length * Math.random() << 0]];
-
-document.write(randomHero.localized_name);
+var randomHero = parsedJson[keys[keys.length * Math.random() << 0]];
 
 var img = document.createElement("img");
-var heroName = randomHero.name.replaceAll("npc_dota_hero_","");
-img.src = "http://cdn.dota2.com/apps/dota2/images/heroes/" + heroName + "_vert.jpg";
-//fix in css
-img.width = 235;
-img.height = 272;
-var src = document.getElementById("hero-image");
+img.id = "hero-image";
+img.src = randomHero.image;
+
+var src = document.getElementById("hero-image-div");
 src.appendChild(img);
+
+var header = document.getElementById("hero-name");
+header.innerText = randomHero.localized_name;
