5cc6946fc644c69044063a27a6080fe92331984d
Mika Nelson <mika_nelson39@mymail.eku.edu>
2020-12-26 15:48:50 -0500

    Changed js file to not call api every reload, updated item API file

--- random_hero.js
+++ random_hero.js
@@ -1,26 +1,35 @@
-function Get(yourUrl) {
+function Get(url) {
   var Httpreq = new XMLHttpRequest(); // a new request
-  Httpreq.open("GET",yourUrl,false);
+  Httpreq.open("GET", url, false);
   Httpreq.send(null);
   return Httpreq.responseText;
 }
 
-function getRandomHero() {
-  var json = Get("https://mikanelson.github.io/heroes/all.json");
-  var parsedJson = JSON.parse(json);
-
-  var keys = Object.keys(parsedJson);
-  var randomHero = parsedJson[keys[keys.length * Math.random() << 0]];
-
-  var img = document.getElementById("hero-image");
-  img.src = randomHero.image;
-
-  var header = document.getElementById("hero-name");
-  header.innerText = randomHero.localized_name;
+function getRandomHero(heroJson) {
+  var keys = Object.keys(heroJson);
+  var randomHero = heroJson[keys[keys.length * Math.random() << 0]];
+  return randomHero;
 }
 
-getRandomHero();
+function updateHeroInformation(randomedHero) {
+  var img = document.getElementById("hero-image");
+  img.src = randomedHero.image;
+  img.addEventListener("load", function () {
+    var header = document.getElementById("hero-name");
+    header.innerText = randomedHero.localized_name;
+  });
+}
+
+function getHeroJsonObj() {
+  var json = Get("https://mikanelson.github.io/heroes/all.json");
+  var parsedJson = JSON.parse(json);
+  return parsedJson;
+}
+
+var heroes = getHeroJsonObj();
+updateHeroInformation(getRandomHero(heroes));
+
 var sortButton = document.getElementById("refresh");
 sortButton.onclick = function refresh() {
-  getRandomHero();
+  updateHeroInformation(getRandomHero(heroes));
 }
