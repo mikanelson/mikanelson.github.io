34635d8766675558a068973153a2a8316a82710c
Mika Nelson <mika_nelson39@mymail.eku.edu>
2020-12-25 22:35:46 -0500

    Added boot API file, updated random_hero to use functions, updated index.html to use ids for image instead of inserting in js

--- random_hero.js
+++ random_hero.js
@@ -1,27 +1,26 @@
 function Get(yourUrl) {
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
+  var Httpreq = new XMLHttpRequest(); // a new request
+  Httpreq.open("GET",yourUrl,false);
+  Httpreq.send(null);
+  return Httpreq.responseText;
 }
 
+function getRandomHero() {
+  var json = Get("https://mikanelson.github.io/heroes/all.json");
+  var parsedJson = JSON.parse(json);
+
+  var keys = Object.keys(parsedJson);
+  var randomHero = parsedJson[keys[keys.length * Math.random() << 0]];
+
+  var img = document.getElementById("hero-image");
+  img.src = randomHero.image;
+
+  var header = document.getElementById("hero-name");
+  header.innerText = randomHero.localized_name;
+}
+
+getRandomHero();
 var sortButton = document.getElementById("refresh");
 sortButton.onclick = function refresh() {
-    location.reload();
+  getRandomHero();
 }
-
-var json = Get("https://mikanelson.github.io/heroes/all.json");
-var parsedJson = JSON.parse(json);
-
-var keys = Object.keys(parsedJson);
-var randomHero = parsedJson[keys[keys.length * Math.random() << 0]];
-
-var img = document.createElement("img");
-img.id = "hero-image";
-img.src = randomHero.image;
-
-var src = document.getElementById("hero-image-div");
-src.appendChild(img);
-
-var header = document.getElementById("hero-name");
-header.innerText = randomHero.localized_name;
